36a63727f05ea008f6fb75380d7eb0f2309c26eb
MarharytaBoichenko <boichenkomv@gmail.com>
2023-05-08 20:47:31 +0300

    feat: add script for copuing  directory in   04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const { mkdir , readdir, copyFile, rm} = require('fs').promises;
 
+const oldDir = path.join(__dirname, 'files')
+const dirPath = path.join(__dirname, 'files-copy')
+
+const copyDir = async () => {
+  try {
+    await rm(dirPath,{
+    recursive: true,
+    force: true,
+  })
+    const newDir = await mkdir(dirPath, { recursive: true })
+    const files = await readdir(oldDir);
+    for (const file of files) {
+      const oldFile = path.join(oldDir, file)
+      const newFile = path.join(newDir, file)
+      copyFile(oldFile, newFile)
+  }
+} catch (error) {
+ console.error(error);
+}
+}
+
+copyDir()
